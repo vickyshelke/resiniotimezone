3083b36dcc993330953e79ac053c077531a2289a
vikramshelke <vickyshelke95@gmail.com>
2017-09-19 19:14:08 +0530

    Update Begin.sh

--- Begin.sh
+++ Begin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-# Default to UTC if no TIMEZONE env variable is set to Turkey
-echo "Setting time zone to ${TIMEZONE=Turkey}"
+# Default to UTC if no TIMEZONE env variable
+echo "Setting time zone to ${TIMEZONE=UTC}"
 # This only works on Debian-based images
 echo "${TIMEZONE}" > /etc/timezone
 dpkg-reconfigure tzdata
