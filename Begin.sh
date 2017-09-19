69c0eec9f8d85427857779c9d6d09132d0d2fa44
vikramshelke <vickyshelke95@gmail.com>
2017-09-19 18:08:33 +0530

    Update Begin.sh

--- Begin.sh
+++ Begin.sh
@@ -5,8 +5,4 @@ echo "Setting time zone to ${TIMEZONE=Turkey}"
 # This only works on Debian-based images
 echo "${TIMEZONE}" > /etc/timezone
 dpkg-reconfigure tzdata
-while : ; do
-    date
-    sleep 60
-done
-
+exit
